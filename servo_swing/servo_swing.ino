f56a207d2b3f7b0eb805aa5cd39f49441677c25a
Numa-io <maanu.106hermitage@gmail.com>
2026-04-10 10:56:38 +0900

    first commit

---
+++ servo_swing/servo_swing.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myservo.attach(2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(i == 0){
+    for(i = 0; i < 180; i ++){
+      myservo.write(i);
+      delay(10);
+    }
+  }
+  if(i > 180){
+    for(i = 180; i > 0; i--){
+      myservo.write(i);
+      delay(10);
+    }
+  }
+}
